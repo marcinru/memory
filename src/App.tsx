b66e84ee7ae68e5b23efa2737cab2235c05fb37c
Marcin Er <mar.rudzki@gmail.com>
2026-01-17 09:34:17 +0100

    add prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+
+const COLORS = ['red', 'green', 'blue', 'yellow'];
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [board, setBoard] = useState(() => [...COLORS, ...COLORS]);
+  const resetGame = () => setBoard([...COLORS, ...COLORS]);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Memory</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {board.map((color, index) => (
+        <div key={index} className="card">
+          {color}
+        </div>
+      ))}
+      <button onClick={() => resetGame}>Reset</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
