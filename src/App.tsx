1d099e22efa0a76cd9a682a0c2704e72a4adea11
Marcin Er <mar.rudzki@gmail.com>
2026-01-17 09:42:05 +0100

    add shuffle function

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ import './App.css';
 const COLORS = ['red', 'green', 'blue', 'yellow'];
 
 function App() {
-  const [board, setBoard] = useState(() => [...COLORS, ...COLORS]);
-  const resetGame = () => setBoard([...COLORS, ...COLORS]);
+  const [board, setBoard] = useState(() => shuffle([...COLORS, ...COLORS]));
+  const resetGame = () => setBoard(shuffle([...COLORS, ...COLORS]));
 
   return (
     <>
@@ -15,9 +15,18 @@ function App() {
           {color}
         </div>
       ))}
-      <button onClick={() => resetGame}>Reset</button>
+      <button onClick={resetGame}>Reset</button>
     </>
   );
+}
+
+function shuffle(array: string[]): string[] {
+  const result = [...array];
+  for (let i = result.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [result[i], result[j]] = [result[j], result[i]];
+  }
+  return result;
 }
 
 export default App;
